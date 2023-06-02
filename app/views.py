7cb8915c241b8f539abbb770bd879af6bc3db871
Jaipalchowdhry <jaipalchowdhary1998@gmail.com>
2023-06-02 16:46:23 -0600

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def suresh(request):
+    return render(request,'suresh.html')
+
+def ali(request):
+    return render(request,'ali.html')
